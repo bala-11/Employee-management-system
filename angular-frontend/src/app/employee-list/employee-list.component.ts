cbe4ed5a90164281b14bf9ca30d39e8c9db79157
bala-11 <balajikalimuthu482@gmail.com>
2023-03-13 13:57:39 +0530

    third commit

---
+++ angular-frontend/src/app/employee-list/employee-list.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { Employee } from '../employee';
+import { EmployeeService } from '../employee.service';
+
+@Component({
+  selector: 'app-employee-list',
+  templateUrl: './employee-list.component.html',
+  styleUrls: ['./employee-list.component.css']
+})
+export class EmployeeListComponent implements OnInit {
+
+  employees:Employee[];
+
+  constructor(private employeeService : EmployeeService) { }
+
+  ngOnInit(): void {
+    this.getEmployees();
+  }
+
+  //   this.employees=[{
+  //     "id": 1,
+  //     "firstName":"Balaji",
+  //     "lastName":" K",
+  //     "emailId":"bala@c.c"
+  //   },
+  // {
+  //   "id": 2,
+  //   "firstName":"Davy jones",
+  //   "lastName":" H",
+  //   "emailId":"davy@c.c"
+  // },
+  // {
+  //   "id": 3,
+  //   "firstName":"Kal El",
+  //   "lastName":" Krypton",
+  //   "emailId":"kidnapsing@c.c"
+  // }];
+private getEmployees(){
+  this.employeeService.getEmployeesList().subscribe(data => {
+this.employees = data;
+  });
+}
+
+}
+
+
